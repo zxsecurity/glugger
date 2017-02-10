8ceda5f16eaccea225b110db7e4b2831b6a2e4c3
Stephen Shkardoon (ss23) <stephen@zxsecurity.co.nz>
2017-02-10 16:15:00 +1300

    Add support for tests

---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+	"reflect"
+)
+
+// TODO: Write required setup for a local resolver with a variety of domains and test cases
+func TestCheckWildcard(t *testing.T) {
+	// A simple test domain -- please don't abuse it
+	ips := checkWildcard("glugger.ss23.geek.nz")
+	expected := []string{"127.0.0.23"}
+	if reflect.DeepEqual(ips, expected) == false {
+		t.Error(
+			"Wildcard detection returned an unexpected result.",
+			"Expected: ", expected,
+			" - Got: ", ips,
+		)
+	}
+
+}
